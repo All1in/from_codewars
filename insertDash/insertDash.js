3d124a77af6cea85d53b0998da1f09ed5074f342
All1in <bm3023068@gmail.com>
2023-04-24 13:42:06 +0300

    square or root js codewars 8 kyu

--- insertDash/insertDash.js
+++ insertDash/insertDash.js
@@ -1,7 +1,7 @@
 const insertDash = num => {
     num = num.toString().split('')
 
-    for(let i = 0; i < num.length; i++){
+    for(let i = 0; i < num.length; i++) {
         if (num[i] % 2 > 0 && num[i + 1] % 2 > 0){
             num[i] += '-'
         }
